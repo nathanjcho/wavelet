72ac9aabad90e6ad11b623de30a68707b776c401
nathanjcho <nathan040403cho@gmail.com>
2022-10-14 20:44:37 -0700

    f

--- NumberServer.java
+++ NumberServer.java
@@ -9,7 +9,8 @@ class Handler implements URLHandler {
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
             return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
+        } 
+        else if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
         } else {
--- SearchEngine.java
+++ SearchEngine.java
@@ -6,34 +6,48 @@ class SearchHandler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     ArrayList<String> words = new ArrayList<String>();
+    ArrayList<String> selectedWords = new ArrayList<String>();
+
+    int numOfWords = 0;
 
     public String handleRequest(URI url) {
         System.out.println("Path: " + url.getPath());
         if (url.getPath().equals("/")) {
-            return String.format("Words: %d", words);
+            return String.format("Number of Strings: %d", numOfWords);
         } 
-        else if (url.getPath().contains("/add")) {
+        else if (url.getPath().contains("/add")) { 
             String[] parameters = url.getQuery().split("=");
-            if (parameters[0].equals("word")) {
-                words.add(parameters[1]);
-                return String.format("Word %s added", parameters[1]);
-            }
-            return "404 Not Found!";
-        }
-        else if (url.getPath().contains("/search")){
+                if (parameters[0].equals("s")) {
+                    String newWord = parameters[1];
+                    words.add((newWord));
+                    numOfWords += 1;
+                    return String.format("Word %d added. Now we have %s words", newWord, numOfWords);
+                }
+            return String.format("Error. Type in a correct format");
+        } 
+        else if (url.getPath().contains("/search")) {
             System.out.println("Path: " + url.getPath());
             String[] parameters = url.getQuery().split("=");
-            int i = 0;
-            if (parameters[0].equals("word")) {
-                for (i = 1; i < words.size(); i++){
-                    if (words.get(i).contains(parameters[1])){
-                        return String.format(parameters[1]);
+                if (parameters[0].equals("s")) {
+                    String searchedWord = parameters[1];
+                    for (String s : words){
+                        if (words.contains(searchedWord)){
+                           selectedWords.add(searchedWord);
+                        }
+                    }
+                    if (selectedWords.size() == 0) {
+                        return String.format("There are no words in the list that contain %d", searchedWord);
+                    }
+                    else {
+                        return selectedWords.toString();
                     }
                 }
-            //  return String.format("Number increased by %s! It's now %d", parameters[1], num);
-            }
-            return "404 Not Found!";
-        }
+            return ("Words found");
+        } 
+        else {
+            System.out.println("Path: " + url.getPath());
+            return "404 Not Found";
+        } 
     }
 }
 
